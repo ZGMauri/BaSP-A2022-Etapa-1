3273cb7664fc09c967ba54898a25bed70cb63ded
ZGMauri <mauri1891.mm@gmail.com>
2022-09-17 19:57:29 -0300

    BaSP-W05-2022: finish exercises part one

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+console.log('----EXERCISE 1 : VARIABLES AND OPERATORS----')
+
+//1 a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+console.log('--EXERCISE 1.a--')
+var numeroUno = 5;
+var numeroDos = 8;
+var resultado = numeroUno + numeroDos;
+console.log(resultado);
+
+//1 b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log('--EXERCISE 1.b--')
+var palabraUno = 'Radium';
+var palabraDos = 'Rocket';
+var concatenarPalabras = palabraUno + palabraDos;
+console.log(concatenarPalabras);
+
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+console.log('--EXERCISE 1.c--')
+var nuevaPalabra = 'Sabado';
+var nuevaPalabraDos = 'Domingo';
+var concatenerMetodoLenght = nuevaPalabra.length + nuevaPalabraDos.length;
+console.log(concatenerMetodoLenght);
